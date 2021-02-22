dbce56df845b3254bc6a11acdd2f7c05ea1e877b
Angger-Raka <78834298+Angger-Raka@users.noreply.github.com>
2021-02-22 10:43:33 +0700

    eps 2 unpas Routing

--- PHPMVC/app/core/App.php
+++ PHPMVC/app/core/App.php
@@ -2,7 +2,16 @@
 
 class App {
     public function __construct(){
-        echo "OK";
-        
+        $url= $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
     }
 }
